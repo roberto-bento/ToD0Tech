447574cf90c704f77b75e92cff18fe4cc4ea5d52
roberto-bento <robertooficial017@gmail.com>
2024-09-16 00:51:31 -0300

    Commit Certa

--- src/main/java/aluno/ifpb/edu/br/ToDoTech/Models/Usuario.java
+++ src/main/java/aluno/ifpb/edu/br/ToDoTech/Models/Usuario.java
@@ -3,6 +3,7 @@ package aluno.ifpb.edu.br.ToDoTech.Models;
 import java.io.Serializable;
 
 public class Usuario implements Serializable {
+    private static final long serialVersionUID = -6819037833242347954L;
     private String nome;
     private String senha;
 
